8af72e4f78ced4ee4fc097cfb8ab12cbc733f268
fermions75 <omifarhan@gmail.com>
2022-01-24 19:45:30 +0600

    flask template add

--- Practice1.py
+++ Practice1.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+
+    {
+        'author' : 'farhan',
+        'title' : 'blog post 1',
+        'content' : 'Blog 1 content',
+    },
+
+    {
+        'author' : 'Omi',
+        'title' : 'blog post 2',
+        'content' : 'Blog 2 content',
+    }
+]
 
 
 @app.route("/")
 def hello_world():
-    return "this is flask page okay"
+    return render_template('Home.html', posts=posts)
 
 @app.route("/about")
 def about_page():
-    return "this is flask page about"
+    return render_template('About.html', title='About')
 
 
 app.run(debug=True)
